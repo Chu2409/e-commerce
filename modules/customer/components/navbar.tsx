021ab688e5ee5b373297f2fbabfaa06ad02f97da
Daniel Zhu <dzhu2409@gmail.com>
2024-03-27 21:09:25 -0500

    Add website icon

--- modules/customer/components/navbar.tsx
+++ modules/customer/components/navbar.tsx
@@ -2,14 +2,22 @@ import Link from 'next/link'
 import { ICategoryRoutes } from '../interfaces/categories-routes'
 import { CustomerMainNav } from './main-nav'
 import { CustomerMobileNavbar } from './mobile-navbar'
+import Image from 'next/image'
 
 export const CustomerNavBar = ({ routes }: { routes: ICategoryRoutes[] }) => {
   return (
     <div className='flex items-center border-b min-h-[3.5rem] max-lg:justify-between'>
       <Link
         href='/'
-        className='text-base font-extrabold transition-colors hover:text-primary text-muted-foreground text-center pr-4 pl-6 max-w-[210px] w-full'
+        className='text-base font-extrabold transition-colors hover:text-primary text-muted-foreground text-center pr-4 pl-6 max-w-[250px] w-full flex items-center gap-4'
       >
+        <Image
+          src='/icon.png'
+          alt='Website Icon'
+          width={24}
+          height={24}
+          className='w-6 h-6 align-middle hover:scale-105 transition-transform duration-200 ease-in-out'
+        />
         D&L American Outlet
       </Link>
 
